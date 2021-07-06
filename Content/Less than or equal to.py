f68047ec452760102c32ee7990eb10a0688ae675
Marc Medrano <83883889+elmarcz@users.noreply.github.com>
2021-07-06 13:42:26 +0200

    Create Less than or equal to.py

---
+++ Content/Less than or equal to.py
@@ -0,0 +1,6 @@
+#To tell if a number or variable is less than or equal to something else, in Phyton we use the symbol ==
+num_one = int(input("Enter 1 number: "))
+num_two = int(input("Enter 2 number: "))
+
+if num_one <= num_two:
+    print("The 1 number is Less than or equal to the second")
