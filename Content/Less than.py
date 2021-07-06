a60e1387129cfbf1d8e3b574bf461354722579bc
Marc Medrano <83883889+elmarcz@users.noreply.github.com>
2021-07-06 13:37:03 +0200

    Create Less than.py

---
+++ Content/Less than.py
@@ -0,0 +1,6 @@
+#To tell if a number or variable is less than something else, in Phyton we use the symbol <
+num_one = int(input("Enter 1 number: "))
+num_two = int(input("Enter 2 number: "))
+
+if num_one < num_two:
+    print("The 1 number is less than the second")
