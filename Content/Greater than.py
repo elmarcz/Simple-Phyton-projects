6ec5ead761a85527dc1ae76180cc72af96d64d4e
Marc Medrano <83883889+elmarcz@users.noreply.github.com>
2021-07-06 13:38:28 +0200

    Create Greater than.py

---
+++ Content/Greater than.py
@@ -0,0 +1,6 @@
+#To tell if a number or variable is greater than something else, in Phyton we use the symbol >
+num_one = int(input("Enter 1 number: "))
+num_two = int(input("Enter 2 number: "))
+
+if num_one > num_two:
+    print("The 1 number is greater than the second")
